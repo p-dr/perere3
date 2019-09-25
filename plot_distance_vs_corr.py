74e0f22d3abaddbb32aac051b29f504a99a72f93
opdo <pedrilidio@gmail.com>
2019-09-25 10:46:29 -0300

    minor.

--- plot_distance_vs_corr.py
+++ plot_distance_vs_corr.py
@@ -1,15 +1,24 @@
 # description: Plots distance from each head to its nearest or overlapped gene as a function of the correlation coeficient between their normalized read counts.
-# in: pardir/'genome_annotation/head_genes_correlations.tsv'
+# in: pardir/'genome_annotation/all_together_now.tsv'
 # out: 
 
-from pandas import read_csv
-from utils import pardir
+import pandas as pd
+from utils import pardir, save_all_figs, GFF3_COLUMNS, unfold_gff
 import matplotlib.pyplot as plt
 from statsmodels.nonparametric.smoothers_lowess import lowess
 
-data = read_csv(pardir/'genome_annotation/head_genes_correlations.tsv', usecols=['distance', 'correlation'], sep='\t')
-data['lowess'] = lowess(data.correlation, data.distance, .6, return_sorted=False)
+#data['lowess'] = lowess(data.correlation, data.distance, .6, return_sorted=False)
+head_data = pd.read_table(pardir/'genome_annotation/all_together_now.tsv',
+                          usecols=['flag', 'strand', 'distance', 'correlation'])
+gene_data = pd.read_table(pardir/'genome_annotation/gene_annotations.gff3',
+                          names=GFF3_COLUMNS, header=None)
+gene_data = unfold_gff(gene_data)
+print(gene_data)
 
+
+### SPLITS
+
+## Strand
 ax = data.plot('distance', 'correlation', 'scatter', alpha=.2)
 data.sort_values('distance').plot.line('distance', 'lowess', ax=ax, color='C1')
 plt.show()

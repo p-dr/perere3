e2a9e82662899b61f561bc851ac76fda38657768
opdo <pedrilidio@gmail.com>
2019-09-25 13:27:56 -0300

    minor.

--- plot_distance_vs_corr.py
+++ plot_distance_vs_corr.py
@@ -9,16 +9,25 @@ from statsmodels.nonparametric.smoothers_lowess import lowess
 
 #data['lowess'] = lowess(data.correlation, data.distance, .6, return_sorted=False)
 head_data = pd.read_table(pardir/'genome_annotation/all_together_now.tsv',
-                          usecols=['flag', 'strand', 'distance', 'correlation'])
+                          usecols=['flag', 'strand', 'distance',
+                                   'correlation', 'neighbor_gene'])
+head_data = head_data.dropna().reset_index()
+
 gene_data = pd.read_table(pardir/'genome_annotation/gene_annotations.gff3',
                           names=GFF3_COLUMNS, header=None)
 gene_data = unfold_gff(gene_data)
-print(gene_data)
-
 
 ### SPLITS
 
 ## Strand
-ax = data.plot('distance', 'correlation', 'scatter', alpha=.2)
-data.sort_values('distance').plot.line('distance', 'lowess', ax=ax, color='C1')
-plt.show()
+neighbor_genes = head_data.neighbor_gene.dropna()
+nei_strand = gene_data.set_index('gene_id').loc[neighbor_genes].reset_index().strand
+same_strand = head_data[head_data.strand == nei_strand]
+diff_strand = head_data.drop(same_strand.index)
+
+## up/down-stream
+# we're restricting to same strand.
+upstream = same_strand[same_strand.strand == '+'
+
+
+## Overlaps or not

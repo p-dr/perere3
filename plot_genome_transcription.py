f3cb8b814c2fe5f9d1cc0b02e835f795e5f7018f
opdo <pedrilidio@gmail.com>
2019-10-10 09:33:20 -0300

    Avanços no relatório.

--- plot_genome_transcription.py
+++ plot_genome_transcription.py
@@ -1,15 +1,27 @@
 import pandas as pd
 import matplotlib.pyplot as plt
 from utils import pardir
+import numpy as np
 
 
-def transc_map(data):
-    seg_data = pd.cut(data
+# def transc_map(data):
+#     seg_data = pd.cut(data
 
 inpath = pardir/'genome_annotation/all_together_now.tsv'
 
 data = pd.read_table(inpath)
 groups = data.groupby('seqid')
 
-for i, group in groups:
-    print(group)
+for name, group in groups:
+    nonzero = group.transcrição.astype('bool').sum()
+    if nonzero < 10:
+        continue
+    group = group.sort_values('start')
+    print('nonzero:', nonzero)
+    print(group[group.transcrição > 0])
+    group.transcrição[group.transcrição > 0].hist(log=True)
+    plt.figure()
+    plt.bar(group.start.values, group.transcrição.values, width=1000,
+            log=True)
+    plt.title(name)
+    plt.show()

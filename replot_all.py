e2aee230d50db8c25132d02757b58a5ad6c917dc
opdo <pedrilidio@gmail.com>
2019-10-22 19:04:51 -0300

    rel

---
+++ replot_all.py
@@ -0,0 +1,13 @@
+from matplotlib.pyplot import close
+from pathlib import mkdir
+
+mkdir('../graficos', exist_ok=True)
+
+
+import plot_distance_vs_corr
+close()
+import plot_corr_motherlength
+close()
+import plot_transcription_hist
+close()
+
